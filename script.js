1026b12d4dbc72c66cdc31d4b3afce788f8d7891
worshipthegoose <rikerfu+thegoose@gmail.com>
2026-05-14 11:03:24 -0700

    2
    ssss

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => { // Getting JS Ready
+    $(document).ready(async () => { // Getting Jquery Ready
+        // Log in console...
+        console.log("JS is ready!")
+        console.log("Jquery is ready!")
+        // A delay function! 
+        function delay(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms)); //
+        }
+        $("#worship-btn").click(async () => {
+            $("smalltext").fadeOut(1000);
+            $("#worship-btn").fadeOut(1000);
+            await delay(1000);
+            $(".aftertext").append("<p>Great! You are now free of your <span class=\"goose\">goos</span>ins!</p>")
+        });
+    });
+});

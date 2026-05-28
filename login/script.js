0678b8b67f0575d1b6620d61b36510c962d9aa01
worshipthegoose <rikerfu+thegoose@gmail.com>
2026-05-28 10:02:53 -0700

    Update script.js

--- login/script.js
+++ login/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
-  const password = "saintrainbowhairs";
+  const password = "mylittlegoselings";
   const inputbox = document.getElementById("inputbox");
   const indicator = document.getElementById("update");
   inputbox.addEventListener('input', function() {

47044c375c3806c5f467532b28be03c20e2f509f
worshipthegoose <rikerfu+thegoose@gmail.com>
2026-05-27 21:31:58 -0700

    Update durex.js

--- durex.js
+++ durex.js
@@ -1,12 +1,15 @@
 // NOTE: THIS IS NOT RELATED TO CONDOMS OR ADULT CONTENT. THIS IS FOR THE PROTECTION SCRIPT AND I THOUGHT THIS WOULD BE FUNNY
-function getCookie(name) {
-  let match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-  if (match) return match[2];
-  return null;
-}
+document.addEventListener("DOMContentLoaded", function(){
 
-      // 2. If the cookie 'site_access' isn't exactly 'granted', kick them out
-if (getCookie('site_access') !== 'granted') {
-  // Change 'login.html' to the exact URL of your login page if it's hosted elsewhere
-  window.location.href = 'login.html'; 
-}
+  function getCookie(name) {
+    let match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    if (match) return match[2];
+    return null;
+  }
+  
+        // 2. If the cookie 'site_access' isn't exactly 'granted', kick them out
+  if (getCookie('site_access') !== 'granted') {
+    // Change 'login.html' to the exact URL of your login page if it's hosted elsewhere
+    window.location.href = 'login.html'; 
+  }
+});

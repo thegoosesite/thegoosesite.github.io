2272f55f266375b8c16517ebfe34ec5a15f34630
worshipthegoose <rikerfu+thegoose@gmail.com>
2026-05-27 20:56:53 -0700

    Create script.js

---
+++ login/script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const password = "saintrainbowhairs";
+  const input = document.getElementById("inputbox");
+  const indicator = document.getElementById("update");
+  function check(){
+    const currentText = input.value;
+    if (currentText === ""){
+      indicator.innerText = "";
+      indicator.className = "indicator";
+      return;
+    } elif (currentText === password) { // It's correct!
+      statusIndicator.textContent = "✔";
+      statusIndicator.className = "indicator correct";
+      setTimeout(() => {
+        document.cookie = "site_access=granted; max-age=" + (60 * 60 * 24 * 7) + "; path=/; SameSite=Strict";
+        window.location.href = 'https://worshipthegoose.github.io/';
+      }, 300);
+    } else {
+      statusIndicator.textContent = "✘";
+      statusIndicator.className = "indicator incorrect";
+    }
+  }
+});

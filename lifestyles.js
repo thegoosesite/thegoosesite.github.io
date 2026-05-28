6d670e184de4a5eb9d85b1649fcc8461cdf875cb
worshipthegoose <rikerfu+thegoose@gmail.com>
2026-05-28 09:56:47 -0700

    Create lifestyles.js

---
+++ lifestyles.js
@@ -0,0 +1,12 @@
+// Decided to go with the condom theme here...
+document.addEventListener("DOMContentLoaded", () => {
+  const logout = document.getElementById("logout");
+  logout.addEventListener("click", function(){
+    document.cookie = "site_access=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Strict"; // Expire cookie
+    // Clear cache..
+    window.localStorage.clear();
+    window.sessionStorage.clear();
+    // Refresh HARD!
+    window.location.reload(true);
+  });
+});

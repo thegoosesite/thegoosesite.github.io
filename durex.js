c0db08f8ef2c73d8adf1aab1e84d27358cd780b9
worshipthegoose <rikerfu+thegoose@gmail.com>
2026-05-29 11:08:14 -0700

    Update durex.js

--- durex.js
+++ durex.js
@@ -1,6 +1,6 @@
 // NOTE: THIS IS NOT RELATED TO CONDOMS OR ADULT CONTENT. THIS IS FOR THE PROTECTION SCRIPT AND I THOUGHT THIS WOULD BE FUNNY
 document.addEventListener("DOMContentLoaded", function(){
-  const IECheck =  !!document.documentMode
+  // const IECheck =  !!document.documentMode (didn't work)
   function getCookie(name) {
     let match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
     if (match) return match[2];
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function(){
   }
   
         // 2. If the cookie 'site_access' isn't exactly 'granted' OR not in IE, kick them out 
-  if (getCookie('site_access') !== 'granted' || !IECheck) {
+  if (getCookie('site_access') !== 'granted') {
     // Change 'login.html' to the exact URL of your login page if it's hosted elsewhere
     window.location.href = 'https://worshipthegoose.github.io/login/'; 
   } 

ede9c4293a160f4d4e0f759cafbccd642fb3ea4c
worshipthegoose <rikerfu+thegoose@gmail.com>
2026-05-28 21:32:23 -0700

    Update script.js

--- login/script.js
+++ login/script.js
@@ -9,32 +9,24 @@ document.addEventListener("DOMContentLoaded", function(){
   
     if (isLetter) {
       console.log(`Letter detected: ${event.key}`);
-      // Process the letter (e.g., custom validation or transformation)
+        // Process the letter (e.g., custom validation or transformation)
       const currentText = inputbox.value;
-    if (currentText === ""){
-      indicator.textContent = "-";
-      indicator.className = "indicator static";
-      return;
-    } else if (currentText === password) { // It's correct!
-      indicator.textContent = "✔";
-      indicator.className = "indicator correct";
-      setTimeout(() => {
-        document.cookie = "site_access=granted; max-age=" + (60 * 60 * 24 * 7) + "; path=/; SameSite=Strict";
-        window.location.href = 'https://worshipthegoose.github.io/';
-      }, 300);
-    } else {
-      indicator.textContent = "✘";
-      indicator.className = "indicator incorrect";
-    }
-  });
-  eye.addEventListener('click', function () {
-    // Toggle the type attribute
-    const type = inputbox.getAttribute('type') === 'password' ? 'text' : 'password';
-    eye.src = eye.getAttribute('src') === 'icons/eye-close-up.png' ? 'icons/eyebrow.png' : 'icons/eye-close-up.png';
-    eye.title = eye.getAttribute('title') === 'Hide Password' ? 'Show Password' : 'Hide Password';
-    inputbox.setAttribute('type', type);
-    
-  });
+      if (currentText === ""){
+        indicator.textContent = "-";
+        indicator.className = "indicator static";
+        return;
+      } else if (currentText === password) { // It's correct!
+        indicator.textContent = "✔";
+        indicator.className = "indicator correct";
+        setTimeout(() => {
+          document.cookie = "site_access=granted; max-age=" + (60 * 60 * 24 * 7) + "; path=/; SameSite=Strict";
+          window.location.href = 'https://worshipthegoose.github.io/';
+        }, 300);
+      } else {
+        indicator.textContent = "✘";
+        indicator.className = "indicator incorrect";
+      }
+
     } else {
       // Optional: Prevent non-letter keys (excluding control keys like Backspace)
       const isControlKey = event.key.length > 1; // e.g., 'Backspace', 'Enter'
@@ -43,6 +35,14 @@ document.addEventListener("DOMContentLoaded", function(){
         event.preventDefault(); 
       }
     }
+    eye.addEventListener('click', function () {
+    // Toggle the type attribute
+    const type = inputbox.getAttribute('type') === 'password' ? 'text' : 'password';
+    eye.src = eye.getAttribute('src') === 'icons/eye-close-up.png' ? 'icons/eyebrow.png' : 'icons/eye-close-up.png';
+    eye.title = eye.getAttribute('title') === 'Hide Password' ? 'Show Password' : 'Hide Password';
+    inputbox.setAttribute('type', type);
+    
+  });
   });
     
 });

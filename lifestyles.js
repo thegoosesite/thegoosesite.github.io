f3aaa688a029abf16cfd975760d05a51fb42e2a7
The Holy Goose <rikerfu+thegoose@gmail.com>
2026-05-31 22:02:45 -0700

    Update lifestyles.js

--- lifestyles.js
+++ lifestyles.js
@@ -1,7 +1,6 @@
 // Decided to go with the condom theme here...
 document.addEventListener("DOMContentLoaded", () => {
   const logout = document.getElementById("logout");
-  const videos = document.querySelector("")
   logout.addEventListener("click", function(){
     document.cookie = "site_access=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Strict"; // Expire cookie
     // Clear cache..

645816ef14756fe50d8265896ddb6cfee173ee0b
The Holy Goose <rikerfu+thegoose@gmail.com>
2026-07-12 17:42:03 -0700

    Update hp-replacement.js

--- hp-replacement.js
+++ hp-replacement.js
@@ -1,17 +1,17 @@
-// NOTE: THIS IS NOT RELATED TO CONDOMS OR ADULT CONTENT. THIS IS FOR THE PROTECTION SCRIPT AND I THOUGHT THIS WOULD BE FUNNY
+// Here is a really cool fact for the 0 people who read this:
+// I actually wanted to make this site freely accessible in Internet Explorer but modern browsers have already
+// depreciated the IE checks so it was impossible.
+// Respect to the users who use IE because they never upgraded from Windows Vista
 document.addEventListener("DOMContentLoaded", function(){
-  // const IECheck =  !!document.documentMode (didn't work)
   const body = document.body;
-  const head = document.head;
   const htmlNormal = `
-        <!-- Header... Sadly there isn't a great way to "snippet" code.-->
         <nav>
             <ul class="grandpa">
                 <li class="dad">
-                    <a class="logo son"href="http://worshipthegoose.github.io"title="Back to the homepage.">The Goose Site</a>
+                    <a class="logo son" href="http://worshipthegoose.github.io" title="Back to the homepage.">The Goose Site</a>
                 </li>
                 <li class="dad">
-                    <a  class="son" title="Sub to @worshiptheholygoose on YT!" href="https://worshipthegoose.github.io/youtube">Youtube Channel</a>
+                    <a class="son" title="Sub to @worshiptheholygoose on YT!" href="https://worshipthegoose.github.io/youtube">Youtube Channel</a>
                 </li>
                 <li class="dad">
                     <a class="son" title="The trinity slideshows..." href="https://worshipthegoose.github.io/powerpoint">Slideshows</a>
@@ -22,12 +22,13 @@ document.addEventListener("DOMContentLoaded", function(){
                 <li class="dad">
                     <a class="son" title="Read the Goose Bible..." href="https://worshipthegoose.github.io/bible">Bible</a>
                 </li>
-                <li class="dad" id="logout"><a class="son" title="Voids Session Data" onclick="document.cookie = 'site_access=granted; expires=Thu, 18 Dec 2026 12:00:00 UTC; path=/;';"><img src="https://worshipthegoose.github.io/signin.png" /><u>Logout</u></a></li>
+                <!-- FIXED: Logout now expires the cookie instantly by setting it to 1970 -->
+                <li class="dad" id="logout"><a class="son" title="Voids Session Data" onclick="document.cookie = 'site_access=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'; window.location.reload();"><img src="https://worshipthegoose.github.io/signin.png" /><u>Logout</u></a></li>
             </ul>
         </nav>
         <main>
             <div class="hello">
-                <center><img id="image" title = "Click me to autorepent" class="worshipper" src="https://worshipthegoose.github.io/frog-goose/goose.png" /></center>
+                <center><img id="image" title="Click me to autorepent" class="worshipper" src="https://worshipthegoose.github.io/frog-goose/goose.png" /></center>
                 <h2 class="heading">Hello there. This is the official website of The <span class="goose">Goose</span>.</h2>
                 <div class="text-region"><p>Welcome to everybody except Saam! This is the holy <span class="goose">Goose</span> typing! This is your one and only reminder to come to St. Duck's church and worship the one and only holy <span class="goose">Goose</span>. Remember, if you don't worship; bad, bad things will happen. If you are a non worshipper, the only way to repent your <span class="goose">goos</span>ins is by pressing that button right there.</p><br></div>
                 <center><button id="repent" title="REPENT!!!">Repent my <span class="goose">goos</span>ins!</button><br>
@@ -47,8 +48,8 @@ document.addEventListener("DOMContentLoaded", function(){
                 </div>
             </div>
             <div class="hello">
-                <center><img style="border-radius:10px;" title = "Saint Rainbowhairs" src="https://worshipthegoose.github.io/frog-goose/dancing-monster.png" /></center>
-                <h1 class="heading" style="-webkit-text-stroke:1px black;background-clip: text;-webkit-background-clip: text;color:transparent;-webkit-text-fill-color: transparent;background-image:linear-gradient(to left, red, blue)">Saint Rainbowhairs</span></h1>
+                <center><img style="border-radius:10px;" title="Saint Rainbowhairs" src="https://worshipthegoose.github.io/frog-goose/dancing-monster.png" /></center>
+                <h1 class="heading" style="-webkit-text-stroke:1px black;background-clip: text;-webkit-background-clip: text;color:transparent;-webkit-text-fill-color: transparent;background-image:linear-gradient(to left, red, blue)">Saint Rainbowhairs</h1>
                 <p>This is Saint Rainbowhairs, known colloqiually as the Dancing Monster.</p>
                 <p>Click the button below to learn more about him.</p><br>
                 <center><button class="izzo" onclick="window.location.href='https://worshipthegoose.github.io/saint-rainbowhairs'" style="font-family:'Comic Sans MS' !important; color:white !important;background-image: linear-gradient(to left, red, blue);border:0px;border-radius:10px;padding:10px;">View full page...</button></center>
@@ -56,8 +57,8 @@ document.addEventListener("DOMContentLoaded", function(){
         </main>
         <footer>
             <h1 class="f1">The Goose Site</h1>
-        </footer>
-    </body>`;
+        </footer>`;
+
   const htmlNSI = `
     <nav style="background-color:darkorange !important; color:white !important;">
       <h1 class="f1">The Goose Site</h1>
@@ -77,12 +78,11 @@ document.addEventListener("DOMContentLoaded", function(){
           <a class="login-a" href="mailto:worshiptheholygoose@gmail.com">Contact Us</a>
         </li>
       </ul>
-        
       </div>
     </nav>
     <main>
         <div class="hello">
-            <center><img title = "I will not give you the passcode..." style="border-radius:10px;" src="https://worshipthegoose.github.io/frog-goose/login.png" /></center>
+            <center><img title="I will not give you the passcode..." style="border-radius:10px;" src="https://worshipthegoose.github.io/frog-goose/login.png" /></center>
             <h1 style="-webkit-text-stroke:1px black;color:darkgreen !important;-webkit-text-stroke:1px white;" title="Login Thank You...">Please Login</h1>
             <p>In order to keep our site secure (from Saam), this site is password-protected.</p>
             <p>We simply ask you to verify that you are a gooseling by entering the holy password.</p>
@@ -90,28 +90,28 @@ document.addEventListener("DOMContentLoaded", function(){
             <div class="input-container">
             <div class="input-stuff"><input style="position:relative !important" type="password" autocomplete="off" readonly onfocus="this.removeAttribute('readonly');" onblur="this.setAttribute('readonly', true);" placeholder="Please input the holy password..." id="inputbox" class="inputbox" title="Input the passcode..."><button type="button" style="display: none;" id="clear" class="inputex">&times;</button></div>
             <p id="update" style="color:silver;" class="update">-</p>
-            <img title="Show Password" class="eye" src="https://worshipthegoose.github.io/login/icons/eyebrow.png"></img>
+            <!-- FIXED: Self closing image tag -->
+            <img title="Show Password" class="eye" src="https://worshipthegoose.github.io/login/icons/eyebrow.png" />
             </div>
             <p style="font-size:12px !important"><u>Password does not contain numbers, symbols, or spaces and are therefore nontypable.</u><br>Secret custom cursors and the navigation bar are also hidden during this period.<br>In progress pages may be accessible with links.</p>
         </div>
     </main>
     <footer>
         <h1 class="f1">The Goose Site</h1>
-    </footer>
-`;
+    </footer>`;
+
   function getCookie(name) {
     let match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
     if (match) return match[2];
     return null;
   }
   
-        // 2. If the cookie 'site_access' isn't exactly 'granted' OR not in IE, kick them out 
-  if (getCookie('site_access') !== 'granted') {
-    document.title = "Sign In - The Goose Site";
-    body.innerHTML = htmlNSI; // Not signed in
-  } else {
+  // Logic evaluation and page generation
+  if (getCookie('site_access') === 'granted') {
     document.title = "The Goose Site";
     body.innerHTML = htmlNormal;
-    head.insertAdjacentHTML('beforeend', `<link rel="stylesheet" href="https://worshipthegoose.github.io/cursor.css" />`);
+  } else {
+    document.title = "Sign In - The Goose Site";
+    body.innerHTML = htmlNSI; 
   }
-});
+}); // FIXED: Added missing closing structure

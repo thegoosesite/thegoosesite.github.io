4f1b209f4200bd364c2f7e9ff0d991a2553728ae
The Holy Goose <rikerfu+thegoose@gmail.com>
2026-07-12 17:43:54 -0700

    Update login.js

--- login.js
+++ login.js
@@ -1,14 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(){
   // const IECheck =  !!document.documentMode (didn't work)
+  // You can see above ^^ that I tried to make the IE check
+  // If this is confusing, please review "hp-replacement.js"
   function getCookie(name) {
     let match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
     if (match) return match[2];
     return null;
   }
   
-        // 2. If the cookie 'site_access' isn't exactly 'granted' OR not in IE, kick them out 
+  // Prevent infinite loop if already on the login page
+  const loginPage = 'https://worshipthegoose.github.io/pages/welcome/';
+  if (window.location.href === loginPage) return;
+
+  // If the cookie 'site_access' isn't exactly 'granted', kick them out 
   if (getCookie('site_access') !== 'granted') {
-    // 3. Redirect user to '/pages/welcome' to sign in with either a one-time code or the master password.
-    window.location.href = 'https://worshipthegoose.github.io/pages/welcome/'; 
+    window.location.href = loginPage; 
   } 
+
 });
